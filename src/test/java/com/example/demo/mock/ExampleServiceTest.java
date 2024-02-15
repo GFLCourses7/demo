ecd5dcec8e29d4dddf6d58eee53e3cc19228d540
artemkedyk <artem.kedyk@uvltd.tech>
2024-02-15 10:59:20 +0200

    - Updated mock test

---
+++ src/test/java/com/example/demo/mock/ExampleServiceTest.java
@@ -0,0 +1,22 @@
+package com.example.demo.mock;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ExampleServiceTest {
+
+    private ExampleRepository exampleRepository = mock(ExampleRepository.class);
+
+    @Test
+    void math() {
+        when(exampleRepository.sum(anyInt(), anyInt())).thenReturn(10);
+        ExampleService exampleService = new ExampleService(exampleRepository);
+        int math = exampleService.math(5, 5);
+        assertEquals(20, math);
+    }
+}
