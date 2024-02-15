c71535032acb774a504674a484a463488968d2f5
artemkedyk <artem.kedyk@uvltd.tech>
2024-02-15 11:04:59 +0200

    - hot fix

--- src/test/java/com/example/demo/mock/ExampleServiceTest.java
+++ src/test/java/com/example/demo/mock/ExampleServiceTest.java
@@ -14,11 +14,8 @@ class ExampleServiceTest {
 
     @Test
     void math() {
-<<<<<<< HEAD
         when(exampleRepository.sum(anyInt(), anyInt())).thenReturn(10);
-=======
         when(exampleRepository.sum(5, 5)).thenReturn(10);
->>>>>>> d0d75d1 (- Added mock test.)
         ExampleService exampleService = new ExampleService(exampleRepository);
         int math = exampleService.math(5, 5);
         assertEquals(20, math);
